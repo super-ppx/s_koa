c78a7f0bccbeb8fbbb80ca055bc1f3c1b7c080d4
super-ppx <307580117@qq.com>
2022-08-28 23:55:22 +0800

    wip

---
+++ src/controllers/auth.ts
@@ -0,0 +1,12 @@
+import { Context } from 'koa'
+
+export default class AuthController {
+    
+    public static async login(ctx: Context){
+        ctx.body = 'Login controller'
+    }
+
+    public static async register(ctx: Context) {
+        ctx.body = 'Register controller'
+    }
+}
