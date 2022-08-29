ed1241b70dc6caa5a792fda219bd4db9368abb28
superman <307580117@qq.com>
2022-08-29 20:18:11 +0800

    wip

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,41 @@
-import Koa from 'koa';
-import cors from '@koa/cors';
-import bodyParser from 'koa-bodyparser';
+import Koa from 'koa'
+import cors from '@koa/cors'
+import bodyParser from 'koa-bodyparser'
+import { createConnection } from 'typeorm'
+import jwt from 'koa-jwt'
+import 'reflect-metadata'
 import { logger } from './logger'
-import router from './routes'
+import { protectedRouter, unprotectedRouter } from './routes'
+import { JWT_SECRET } from './constants'
 
-// 初始化 Koa 应用实例
-const app = new Koa();
 
-// 注册中间件
-app.use(logger())
-app.use(cors());
-app.use(bodyParser());
+createConnection()
+    .then(() => {
+        // 初始化 Koa 应用实例
+        const app = new Koa()
 
-// 响应用户请求
-app.use(router.routes()).use(router.allowedMethods());
+        // 注册中间件
+        app.use(logger())
+        app.use(cors())
+        app.use(bodyParser())
 
-// 运行服务器
-app.listen(3000);
+        app.use(async (ctx, next) => {
+            try {
+                await next()
+            } catch (err) {
+                // 只返回 JSON 格式的响应
+                ctx.status = err.status || 500
+                ctx.body = { message: err.message }
+            }
+        })
+
+        // 响应用户请求
+        app.use(unprotectedRouter.routes()).use(unprotectedRouter.allowedMethods())
+        app.use(jwt({ secret: JWT_SECRET }).unless({ method: 'GET' }))
+        app.use(protectedRouter.routes()).use(protectedRouter.allowedMethods())
+
+        // 运行服务器
+        app.listen(3000)
+    })
+    .catch((err: string) => console.log('TypeORM connection error: ', err))
+
